30ad0f142af2d8956ddca73045db432718d59ffc
Alexey <kalnitskiy2018@mail.ru>
2023-07-16 13:27:38 +0500

    ex2

---
+++ ex2/Program.cs
@@ -0,0 +1,16 @@
+﻿System.Console.WriteLine("Введите числа: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
+if(a > b && a > c){
+    System.Console.WriteLine("Наибольшее число: "+ a);
+}
+else if (a < b && c < b){
+        System.Console.WriteLine("Наибольшее число: "+ b);
+}
+else if(c > a && c > b){
+    System.Console.WriteLine("Наибольшее число: "+ c);
+}
+else{
+    System.Console.WriteLine("Попробуйте еще раз");
+}
