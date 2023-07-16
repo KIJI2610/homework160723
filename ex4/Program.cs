6984df94608d6a4916d85eb2ff0f92e01c3f7ab6
Alexey <kalnitskiy2018@mail.ru>
2023-07-16 13:34:16 +0500

    ex4

---
+++ ex4/Program.cs
@@ -0,0 +1,7 @@
+﻿System.Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+for(int i = 0; n >= i; i++){
+    if (i % 2 == 0){
+        System.Console.WriteLine(i);
+    }
+}
