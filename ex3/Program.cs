478dd5bfe0620ba232815e210134bfd5f6668b35
Alexey <kalnitskiy2018@mail.ru>
2023-07-16 13:30:56 +0500

    ex3

---
+++ ex3/Program.cs
@@ -0,0 +1,8 @@
+﻿System.Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n % 2 == 0){
+    System.Console.WriteLine("Является");
+}
+else{
+    System.Console.WriteLine("Не является");
+}
